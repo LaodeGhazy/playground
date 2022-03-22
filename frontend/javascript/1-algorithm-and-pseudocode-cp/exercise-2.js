9da07f7ef7e45d1dc1c545123c0789047ab1f701
LaodeGhazy <68162980+LaodeGhazy@users.noreply.github.com>
2022-03-22 15:27:34 +0700

    Update exercise-2.js

--- frontend/javascript/1-algorithm-and-pseudocode-cp/exercise-2.js
+++ frontend/javascript/1-algorithm-and-pseudocode-cp/exercise-2.js
@@ -2,3 +2,7 @@
 // Seseorang diperbolehkan membuat SIM jika usianya lebih besar atau sama dengan 17 tahun.
 
 // TODO: answer here
+IF (usia>17)
+  output "Boleh dapat SIM"
+ELSE
+output "Tidak Boleh Dapat SIM"
